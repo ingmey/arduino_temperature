db83165a38b68396771749c595fed969f9cf74ad
Ingvar <ingvar@blue>
2020-09-26 16:13:45 +0200

    change serial communication init parameter

--- pc/serialportinit.py
+++ pc/serialportinit.py
@@ -7,8 +7,8 @@ def init_port(port):
     ser.baudrate = 9600
     ser.timeout = 5               #non-block read
     #ser.timeout = 2              #timeout block read
-    ser.xonxoff = False     #disable software flow control
-    ser.rtscts = False     #disable hardware (RTS/CTS) flow control
-    ser.dsrdtr = False       #disable hardware (DSR/DTR) flow control
+    #ser.xonxoff = False     #disable software flow control
+    #ser.rtscts = False     #disable hardware (RTS/CTS) flow control
+    #ser.dsrdtr = False       #disable hardware (DSR/DTR) flow control
     return ser;
 
