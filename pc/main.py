4f3b9e9a28cf0634923a789cb96498bef30438f8
Ingvar <ingvar@blue>
2020-09-30 21:20:19 +0200

    improve startup of measurement

--- pc/main.py
+++ pc/main.py
@@ -1,9 +1,8 @@
 # Main program for logging temperature
 import datetime, time 
 import checkPortConnect
-import serialportinit
 import readonserieport
-import signal
+import serial
 import sys
 
 device = '/dev/ttyACM0'
@@ -14,25 +13,38 @@ if __name__ == "__main__":
     while True:
         ret = checkPortConnect.check_presence(device)
         if ret:
-            serialport = serialportinit.init_port(device)
-            #time.sleep(2 * 60) # wait for port to b ready
+            print "Arduino is connected "
+            print "Setup serial port to Arduino"
+            ser = serial.Serial()
+            ser.port = device
+            ser.baudrate = 9600
+            ser.timeout = 5
+            print "Wait 1,5 minutes for the Arduino to connect to the serial port"
+            time.sleep(90) # wait for port to be ready about 2 minutes
             try:
-                serialport.open()
-                print "serial port open"
-            except serial.serialutil.SerialException:
-                print "could not open serial port open " + e
+                ser.open()
+                print "serial port has been open"
+            except Expection, e: #serial.serialutil.SerialException
+                print ("could not open serial port") + str(e)
                 quit()
             today = datetime.datetime.now()
             todaydate1 =today.strftime("%Y-%m-%d")
             print("Date: " + todaydate1)
-            while serialport.is_open:
-                time.sleep(5) # sleep 5 seconds
-                today = datetime.datetime.now()
-                todaydate2 =today.strftime("%Y-%m-%d")
-                if todaydate1 != todaydate2:
-                    todaydate1 = todaydate2
-                    print("Date: " + todaydate1)
-                ret = readonserieport.read_temperature(serialport)
-                if ret:
-                    print ret
-            serialport.close()
+            while ser.is_open:
+                try:
+                    ser.flushInput()
+                    ser.flushOutput()
+                    time.sleep(5) # sleep 5 seconds
+                    today = datetime.datetime.now()
+                    todaydate2 =today.strftime("%Y-%m-%d")
+                    if todaydate1 != todaydate2:
+                        todaydate1 = todaydate2
+                        print("Date: " + todaydate1)
+                    ret = readonserieport.read_temperature(ser)
+                    if ret:
+                        print ret
+                except (KeyboardInterrupt, SystemExit):
+                    ser.close()
+                    quit()
+                    
+            ser.close()
